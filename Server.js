b7c44275c21687d79956f06d1c3bcd4804153439
bhagwat-jadhav <bhagwatjadhav649@gmail.com>
2025-07-18 18:52:24 +0530

    Add process.env.port

--- Server.js
+++ Server.js
@@ -2,12 +2,13 @@ const express= require("express");
 const path= require("path");
 const app = express ();
 app.use(express.json());
+const port= process.env.port || 3000;
 app.get('\login',(req,res)={
         res.sendFile(path.join(__dirname,'index.html');
 });
 app.post('\login',(req,res)={
          res.json(req.body);
 });
-app.listen(3000,()={
+app.listen(port,()={
            console.log('server running at port 3000');
 });
