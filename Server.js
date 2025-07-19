bb48b4eacba6c4c4176081183570008b19c2fba8
bhagwat-jadhav <bhagwatjadhav649@gmail.com>
2025-07-19 09:01:14 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,12 +1,11 @@
 const express= require("express");
 const path= require("path");
 const app = express ();
-app.use(express.json());
-const port= process.env.port || 3000;
-app.get('\login',(req,res)={
+const port= process.env.PORT || 3000;
+app.get('/login',(req,res)={
         res.sendFile(path.join(__dirname,'index.html');
 });
-app.post('\login',(req,res)={
+app.post('/login',(req,res)={
          res.json(req.body);
 });
 app.listen(port,()={
